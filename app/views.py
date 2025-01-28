4102aaf7712782a4fe3300dad64262d4372d60e6
mohammedsabri11 <mohammed211920@gmail.com>
2025-01-28 21:53:31 +0300

    Initial commit key

--- app/views.py
+++ app/views.py
@@ -55,9 +55,9 @@ def generate_floor_plan(request):
         )
 
         # Call DALL-E API (replace with your actual API call)
-     
+        api_key = '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'
         headers = {
-            "Authorization": f"Bearer YOUR_API_KEY",
+            "Authorization": f"Bearer {app_key}",
             "Content-Type": "application/json"
         }
         data = {
